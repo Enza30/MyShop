6d9550c5c897fddc2686cc99a6cbc7bce1a9c966
Farendza Muda <farendza.muda@gmail.com>
2022-09-21 18:05:20 +0700

    Initial Commit

---
+++ MyShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyShop
+//
+//  Created by Farendza Muda on 21/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyShop/MyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShopApp.swift
+//  MyShop
+//
+//  Created by Farendza Muda on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
